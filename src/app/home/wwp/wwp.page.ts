b4346a190d50f9c9174e0beb01b3f7803c677453
himeriusnico <himeriusnico03@gmail.com>
2024-09-10 13:27:37 +0700

    WWP 3.5/4 done
    Masih bingung cara round nya dan bingung cara right align button

--- src/app/home/wwp/wwp.page.ts
+++ src/app/home/wwp/wwp.page.ts
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wwp.page.scss'],
 })
 export class WwpPage implements OnInit {
+  games = [
+    {
+      name: 'Valorant',
+      description: 'Valorant is a tactical first-person shooter from Riot Games that combines precise gunplay with unique character abilities. Players engage in strategic, team-based matches where they must complete objectives and outsmart opponents to win.',
+      image: 'assets/img/valorant.jpg',
+      alt: 'Valorant',
+    },
+    {
+      name: 'Mobile Legends',
+      description: 'Mobile Legends is a multiplayer online battle arena (MOBA) developed by Moonton. Players engage in 5v5 battles using a variety of heroes, aiming to destroy the enemy\'s base while defending their own.',
+      image: 'assets/img/mobile-legends.jpg',
+      alt: 'Mobile Legends',
+    }
+  ];
 
   constructor() { }
 

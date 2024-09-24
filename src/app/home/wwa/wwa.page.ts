b372f57dc0c50e363359a2a8bd1b62167e710dd6
Ryunite <lazyryu12@gmail.com>
2024-09-24 14:04:05 +0700

    WWA fix
    Ganti tampilan pakai ngFor + Image ganti dari local

--- src/app/home/wwa/wwa.page.ts
+++ src/app/home/wwa/wwa.page.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Description {
+  namaTeam:String;
+  deskripsiTeam: String;
+}
+
 @Component({
   selector: 'app-wwa',
   templateUrl: './wwa.page.html',
   styleUrls: ['./wwa.page.scss'],
 })
 export class WwaPage implements OnInit {
+
+  arrDesc:Description[] = [
+    {
+      namaTeam: "MahaSigma",
+      deskripsiTeam: "Titan Strike is a competitive e-sports team known for their telentless drive "
+      + "and tactical prowess across multiple gaming titles, especially in Valorant and"
+      + "League of Legend With a roster of skilled players and a reputation for teamwork"
+      + "and strategic excellence."
+      + "Titan Strike has earned top placements in regional"
+      + "and international tournaments. Their commitment to innovation and growth continues"
+      + "to make them a formidable force in the e-sports arena."
+    }
+  ]
   numberOfLikeClicked:number=0;
   constructor() { }
 

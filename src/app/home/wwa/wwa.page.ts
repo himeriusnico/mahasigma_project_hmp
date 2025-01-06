cb090d3f5c54edbf8d22b4580a2a0a6f166621a2
Ryunite <lazyryu12@gmail.com>
2025-01-06 23:15:37 +0700

    Adjust WWA

--- src/app/home/wwa/wwa.page.ts
+++ src/app/home/wwa/wwa.page.ts
@@ -15,11 +15,11 @@ export class WwaPage implements OnInit {
   arrDesc:Description[] = [
     {
       namaTeam: "MahaSigma",
-      deskripsiTeam: "Titan Strike is a competitive e-sports team known for their telentless drive "
+      deskripsiTeam: "Mahasigma is a competitive e-sports team known for their telentless drive "
       + "and tactical prowess across multiple gaming titles, especially in Valorant and"
       + "League of Legend With a roster of skilled players and a reputation for teamwork"
       + "and strategic excellence."
-      + "Titan Strike has earned top placements in regional"
+      + "Mahasigma has earned top placements in regional"
       + "and international tournaments. Their commitment to innovation and growth continues"
       + "to make them a formidable force in the e-sports arena."
     }

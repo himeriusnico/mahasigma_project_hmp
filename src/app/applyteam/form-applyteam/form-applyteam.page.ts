d5157d12aab78a85482631c8ef4dfcefe397ec60
Ryunite <lazyryu12@gmail.com>
2025-01-06 23:13:49 +0700

    Apply Service to Form Apply Team

--- src/app/applyteam/form-applyteam/form-applyteam.page.ts
+++ src/app/applyteam/form-applyteam/form-applyteam.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { EsportService } from 'src/app/esport.service';
 
 @Component({
   selector: 'app-form-applyteam',
@@ -7,37 +8,61 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./form-applyteam.page.scss'],
 })
 export class FormApplyteamPage{
-  games = [
-    { name: 'Soccer', teams: ['Team A', 'Team B', 'Team C'] },
-    { name: 'Basketball', teams: ['Team X', 'Team Y', 'Team Z'] },
-    { name: 'Cricket', teams: ['Team Alpha', 'Team Beta'] },
-  ];
-
   selectedGame: any = null;
-  teams: string[] = [];
-  selectedTeam: string | null = null;
-  reason: string = '';
+  selectedTeam: any = null;
+  arrGame: any[] = []
+  arrTeam: any[] = []
+  description: string = '';
 
-  constructor(private navCtrl: NavController) {}
+  constructor(
+    private navCtrl: NavController,
+    private esportservice: EsportService
+  ) {}
+
+  ngOnInit() {
+    this.esportservice.getgame().subscribe(
+      (response) => {
+        if (response.result === 'success') {
+          this.arrGame = response.data
+        }
+        else {
+          alert(response.message)
+        }
+      }
+    )
+  }
 
   // Update teams dropdown based on the selected game
-  onGameChange(event: any) {
-    const selected = this.games.find((game) => game.name === this.selectedGame.name);
-    this.teams = selected ? selected.teams : [];
-    this.selectedTeam = null; // Reset the team selection
+  onGameChange() {
+    const selectedgameid = this.selectedGame
+
+    this.esportservice.getteam(selectedgameid).subscribe(
+      (response: any) => {
+        if (response.result === 'success') {
+          this.arrTeam = response.data
+        }
+        else {
+          this.arrTeam = []
+        }
+      }
+    )
   }
 
   // Handle form submission
   submitApplication() {
-    console.log('Application Submitted:', {
-      game: this.selectedGame.name,
-      team: this.selectedTeam,
-      reason: this.reason,
-    });
-    alert('Application submitted successfully!');
+    const currentUser = JSON.parse(localStorage.getItem('currentUser') || '{}');
+    const idmember = currentUser[0]?.idmember || null;
 
-    // Navigate back to the home page after submission
-    this.navCtrl.navigateBack('/home');
+    this.esportservice.newproposal(idmember, this.selectedTeam, this.description).subscribe(
+      (response: any) => {
+        if (response.result === 'success') {
+          alert(response.message)
+          this.navCtrl.navigateBack('/applyteam');
+        }
+        else {
+          alert(response.message)
+        }
+      }
+    )
   }
-
 }
